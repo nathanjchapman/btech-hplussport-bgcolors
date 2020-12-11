6811084c6f1b134e3d7f2c9de8872fa17afd6991
Nathan Chapman <nathanchapman@protonmail.com>
2020-12-10 19:19:40 -0700

    Add jQuery filters

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,34 @@
 $("document").ready(function() {
-	$("[data-type='vitamin']").css("background-color", "green");
-	$("[data-type='mineralwater']").css("background-color", "blue");
-	$("[data-type='proteinbar']").css("background-color", "RebeccaPurple");
+    $("[data-type='vitamin']").css("background-color", "green");
+    $("[data-type='mineralwater']").css("background-color", "blue");
+    $("[data-type='proteinbar']").css("background-color", "RebeccaPurple");
+
+    // filter
+    document.querySelector("#filter-vitamins").addEventListener("change", function(evnt) {
+        updateProductFilter("vitamins", evnt.target.checked);
+    });
+    document.querySelector("#filter-mineralwater").addEventListener("change", function(evnt) {
+        updateProductFilter("waters", evnt.target.checked);
+    });
+    document.querySelector("#filter-proteinbars").addEventListener("change", function(evnt) {
+        updateProductFilter("bars", evnt.target.checked);
+    });
+
+    const updateProductFilter = (filter, state) => {
+        let val = "";
+
+        switch (filter) {
+            case "vitamins":
+                val = "[data-type='vitamin']";
+                break;
+            case "waters":
+                val = "[data-type='mineralwater']";
+                break;
+            case "bars":
+                val = "[data-type='proteinbar']";
+                break;
+        }
+
+        $(".product-item").has(val).css('display', state ? "" : "none");
+    };
 });
