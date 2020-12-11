28e5e914b1f6a811ddb1ee9fcc0a301721dcf20d
Nathan Chapman <nathanchapman@protonmail.com>
2020-12-10 17:49:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# HPlus Sport Background Image change
+
+Write jqeury that dynamically changes the background colors of the products
+
+- vitamin: green
+- mineralwater: blue
+- proteinbar: dark purple
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+$("document").ready(function() {
+	$("[data-type='vitamin']").css("background-color", "green");
+	$("[data-type='mineralwater']").css("background-color", "blue");
+	$("[data-type='proteinbar']").css("background-color", "RebeccaPurple");
+});
